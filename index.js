a92eef335115f970b4c326ca89cff2dff91e231d
skillsept <franck.soubieres+skillsept@gmail.com>
2026-03-29 18:30:40 +0200

    v1

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 module.exports = {
   sc_plugin_api_version: 1,
+  plugin_name: "my_plugin",
 
-  onLoad: async () => {
-    console.log("Plugin minimaliste chargé !");
+  // Exemple : ajouter un type de champ
+  fieldviews: {
+    my_custom_view: {
+      isEdit: true,
+      run: (field_name, value, attrs, cls) => {
+        return `<input type="text" name="${field_name}" value="${value || ""}" class="${cls}">`;
+      }
+    }
   }
 };
